d0e0574f0ba3bb5a36640c4f18b6db6b0d8e6e30
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2021-06-21 16:51:17 +0300

    fix mongo

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const app = express(); // initialize your express app instance
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT;
-const seedUserData = require('./models/user.model');
+// const seedUserData = require('./models/user.model');
 const cors = require('cors'); // enable the communication between the frontend and the backend
+// const booksSchema = require('./models/books.model');
 
 
 app.use(cors());
 
 
-mongoose.connect('mongodb://localhost:27017/myFavoriteCats',
+mongoose.connect('mongodb://localhost:27017/myFavoriteBooks',
 { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
@@ -22,9 +23,30 @@ app.get('/', (req, res) => {
     res.send('Your API Server is running now');
 });
 
-app.get('/books', getBooks )
+app.get('/books', getBooks );
 
-seedUserData();
+// const userSchema = new mongoose.Schema({
+//     email: { type: String },
+//     books: [booksSchema]
+// });
+
+// const userModel = mongoose.model('users', userSchema);
+
+// const seedUserData = () => {
+//     const newUser = new userModel({
+//         email: 'ibrahimalhamshari742@gmail.com',
+//         books: [
+//             { name: 'ASME Book' , description:' For mechanical engineering', status:'Available'},
+//             { name: 'API Book' , description:' For mechanical engineering', status:'Available'},
+//             { name: 'Python Book' , description:' For software engineering', status:'Available'}
+//         ]
+
+//     });
+
+//     newUser.save();
+// }
+
+// seedUserData();
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
